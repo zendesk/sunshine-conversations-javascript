24b2df13258fd24441ece3320379a7c070c056bd
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-11-04 16:56:52 -0500

    dont ignore src for those who want to integrate it fully to their build

--- src/smooch.js
+++ src/smooch.js
@@ -1,10 +1,12 @@
 import { AppUsersAPI } from './api/appUsers';
 import { ConversationsAPI } from './api/conversations';
+import packageInfo from '../package.json';
 
 const SERVER_URL = 'https://api.smooch.io/v1';
 
 export class Smooch {
   constructor(serverURL = SERVER_URL) {
+    this.VERSION = packageInfo.version;
     this.serverURL = serverURL;
 
     this.appUsers = new AppUsersAPI({
