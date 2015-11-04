0448e886a87119138b6bc63f239e3943ca09a8f8
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-11-04 08:48:40 -0500

    added full test suite for webhooks, still some failures

--- src/api/webhooks.js
+++ src/api/webhooks.js
@@ -2,58 +2,57 @@ import { BaseAPI } from './base';
 import { http } from '../utils/http';
 
 export class WebhooksAPI extends BaseAPI {
+  validateAuth(headers) {
+    return headers && headers.Authorization ? Promise.resolve(headers) : Promise.reject(new Error('Must provide a JWT.'));
+  }
+
+  validateProps(props, isTargetRequired = false) {
+    if (!props || Object.keys(props).length === 0) {
+      return Promise.reject(new Error('Must provide props'));
+    }
+
+    if (isTargetRequired && !props.target) {
+      return Promise.reject(new Error('Must provide a target'));
+    }
+
+    return Promise.resolve(props);
+  }
+
   list(auth) {
     const url = this.getFullURL('webhooks');
-    return this.getAuthenticationHeaders(auth).then((authHeaders) => {
+    return this.getAuthenticationHeaders(auth).then(this.validateAuth).then((authHeaders) => {
       return http('GET', url, {}, authHeaders);
     });
   }
 
-  create(target, eventName, auth) {
+  create(props, auth) {
     const url = this.getFullURL('webhooks');
-    return this.getAuthenticationHeaders(auth).then((authHeaders) => {
-      let attributes = {
-        target: target
-      };
-
-      if (eventName) {
-        attributes.event = eventName;
-      }
-
-      return http('POST', url, attributes, authHeaders);
+    return this.getAuthenticationHeaders(auth).then(this.validateAuth).then((authHeaders) => {
+      return this.validateProps(props, true).then((validatedProps) => {
+        return http('POST', url, validatedProps, authHeaders);
+      });
     });
   }
 
   get(webhookId, auth) {
     const url = this.getFullURL('webhooks', webhookId);
-    return this.getAuthenticationHeaders(auth).then((authHeaders) => {
+    return this.getAuthenticationHeaders(auth).then(this.validateAuth).then((authHeaders) => {
       return http('GET', url, {}, authHeaders);
     });
   }
 
-  update(webhookId, target, eventName, auth) {
-    if (!target && !eventName) {
-      return Promise.reject(new Error('Must provide at least one thing to update.'));
-    }
-
+  update(webhookId, props, auth) {
     const url = this.getFullURL('webhooks', webhookId);
-    return this.getAuthenticationHeaders(auth).then((authHeaders) => {
-      let attributes = {};
-
-      if (eventName) {
-        attributes.event = eventName;
-      }
-      if (target) {
-        attributes.target = target;
-      }
-
-      return http('PUT', url, attributes, authHeaders);
+    return this.getAuthenticationHeaders(auth).then(this.validateAuth).then((authHeaders) => {
+      return this.validateProps(props).then((validatedProps) => {
+        return http('PUT', url, props, authHeaders);
+      });
     });
   }
 
   delete(webhookId, auth) {
     const url = this.getFullURL('webhooks', webhookId);
-    return this.getAuthenticationHeaders(auth).then((authHeaders) => {
+    return this.getAuthenticationHeaders(auth).then(this.validateAuth).then((authHeaders) => {
       return http('DELETE', url, undefined, authHeaders);
     });
   }
