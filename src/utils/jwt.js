6bc24612f6efbede8efd459b4c109800c927a3b6
hyjaz <aloudin@zendesk.com>
2020-10-15 13:37:10 -0400

    remove all

--- src/utils/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-export function generate(body, secret, keyId) {
-    return jwt.sign(body, secret, {
-        headers: {
-            kid: keyId
-        }
-    });
-}
